d5fbfdab6d8eb20884ffa86f279865bc77d905d4
edwardcole <edward.peter.cole@gmail.com>
2022-11-18 23:15:50 -0800

    basic implementation 😼😼😼😼 (supporting saving and clicks)
    This first version of our extension includes the following features:
        1. A small pop-up with a game.
        2. Local saving for the game with chrome.storage.local.
    The game is a simple clicker game which I stylized with the CSS found inside main.css.
    The code for the game and the saving feature can be found within click.js.
    The pop-up can be found in popup.html.
    It's important to note that all of these are found within the main folder that contains everything.
    
    Every click results in a call to chrome.storage to save. This didn't seem to be an issue within testing. Later, we can debounce if needed, or make use of an on close event.

--- click.js
+++ click.js
@@ -1,20 +1,16 @@
 const owls = document.getElementById("owls")
 const owl = document.getElementById("owl")
 let owlCount = 0
-chrome.storage.local.get(['owls'], result => {
-    if (result.key != undefined) {
-        owlCount = result.key
+chrome.storage.local.get(['owls'], function(result) {
+    if (result.owls != undefined) {
+        owlCount += result.owls
+        owls.innerHTML = "obese owls: "+owlCount
     }
 })
 function click() {
     owlCount += 1
     owls.innerHTML = "obese owls: "+owlCount
     owl.style.transform = "rotate("+(Math.random()*2-1)*5+"deg)"
+    chrome.storage.local.set({owls: owlCount})
 }
 owl.onclick = click
-
-setInterval(() => {
-    chrome.storage.local.set({owls: owlCount}, () => {
-        console.log("saved owls with value "+owlCount)
-    })
-},5000)
