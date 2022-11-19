47ea6bf1f80ead4f59a8a134a97c885586722480
edwardcole <edward.peter.cole@gmail.com>
2022-11-18 21:37:51 -0800

    graah

--- click.js
+++
@@ -1,8 +0,0 @@
-const owls = document.getElementById("owls")
-const owl = document.getElementById("owl")
-let owlCount = 0
-function click() {
-    owlCount += 1
-    owls.innerHTML = "obese owls: "+owlCount
-}
-owl.onclick = click
